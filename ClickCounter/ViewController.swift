542f24a89e72c3d637883d1281a8025a39935251
Dan Huang <jhyqt5@gmail.com>
2016-06-09 10:12:22 -0600

    feat: Add counter function

--- ClickCounter/ViewController.swift
+++ ClickCounter/ViewController.swift
@@ -10,12 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
+    
     @IBOutlet weak var counterLabel: UILabel!
     @IBOutlet weak var startButtonLabel: UIButton!
+    
+
+    var counter = 0
+    var timer =  NSTimer()
+    
     @IBAction func startCounter(sender: AnyObject) {
+
+        counter = counter + 1
+        counterLabel.text = String(counter)
+        
     }
-
-
 }
 
