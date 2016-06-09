bda0334d083c6b4cfddd1f7badcf648990caf1f2
Dan Huang <jhyqt5@gmail.com>
2016-06-09 09:43:44 -0600

    feat: Connect outlets

--- ClickCounter/ViewController.swift
+++ ClickCounter/ViewController.swift
@@ -10,14 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var startButtonLabel: UIButton!
+    @IBAction func startCounter(sender: AnyObject) {
     }
 
 
